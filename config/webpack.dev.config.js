58c073aa7d9917f691b349466da523937a0fa176
xiechao <xiechao1211@126.com>
2018-11-05 22:40:09 +0800

    排序更新

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -9,7 +9,19 @@ module.exports = merge(baseWebpackConfig, {
     hot: true,
     host: process.env.HOST || '0.0.0.0',
     port: process.env.PORT || '8000',
-    publicPath: '/'
+    publicPath: '/',
+    proxy: {
+        '/api': {
+            //去除环境变量的双引号
+            target: 'https://www.jianshu.com',
+            // target: 'http://118.31.176.215:9999',
+            // target: prod.BASE_API.replace(/\"/g, '') + '/api',
+            changeOrigin: true,
+            pathRewrite: {
+                '^/api': '/'
+            }
+        }
+    }
   },
   // webpack dev模式下的插件配置
   plugins: [
